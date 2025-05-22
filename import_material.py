e8e22db31c3e68800c7ef79c233d4fb5df8918b0
Balraj Singh Hada <balraj.singh@faberwork.com>
2025-05-22 17:42:05 +0530

    readme.md done

---
+++ import_material.py
@@ -0,0 +1,18 @@
+import os
+import django
+import csv
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourproject.settings")
+django.setup()
+
+from tools.models import Material
+
+with open("materials.csv", newline='') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        Material.objects.create(
+            name=row["Material Name"],
+            formula=row["Scientific Formula"],
+            co2_intensity=row["CO2 Intensity (kg CO2 per kg of material)"]
+        )
+print("Materials imported.")
--- readme.md
+++ readme.md
@@ -1,2 +1,9 @@
 pip install -r requirements.txt
+
+python manage.py migrate
+
+python manage.py shell
+in shell type exec(open("import_material.py").read())
+
 python manage.py runserver 3003
+
