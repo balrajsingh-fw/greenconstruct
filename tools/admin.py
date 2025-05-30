fdae8ce3fbcf32846e3575a794ef7dd981f68adf
Balraj Singh Hada <balraj.singh@faberwork.com>
2025-05-30 15:07:59 +0530

    documents added to django admin

--- tools/admin.py
+++ tools/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Material, Project, BuildingType
+from .models import Material, Project, BuildingType, Document
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Material)
 admin.site.register(BuildingType)
+admin.site.register(Document)
